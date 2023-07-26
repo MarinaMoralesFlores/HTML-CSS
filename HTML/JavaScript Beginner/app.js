6e6db49cd82227ba436e76178dbbfb3471d3598d
MarinaMoralesFlores <marinamorales98@gmail.com>
2023-07-27 00:21:41 +0200

    clase

--- HTML/JavaScript Beginner/app.js
+++ HTML/JavaScript Beginner/app.js
@@ -1,2 +1,63 @@
 console.log('Hello, World')
 
+let numero = 838 < 542
+console.log(numero)
+
+let numero2 = 17/5
+console.log(numero2)
+
+let numero3 = 12 * 6 
+console.log(numero3)
+
+let numero4 = 35%8
+
+console.log(numero4)    
+
+
+ 
+
+let numero5 = 1 == 3 || 5>7 || 10*5 > 40
+
+console.log(numero5)
+
+
+ 
+
+let numero6 = 13% 2 == 0
+
+console.log(numero6)
+
+
+ 
+
+let numero7 = "CODE" + "" + "SPACE"
+
+console.log(numero7)  
+
+
+ 
+
+let numero8 = (21/2) %3
+
+console.log(numero8)
+
+
+ 
+
+let numero9 = 5 == "5"
+
+console.log(numero9)  
+
+let numero10 = !(1 == 1)
+
+console.log(numero10)
+a. Boolean
+b. 3.4. Numero
+c. Numero. 72
+d  3 numero
+e true - Boolean
+f false Boolean
+g  CODESPACE string
+h 1.5 numero
+i true Boolean
+j false Boolean
