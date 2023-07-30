97e573f8ef25afc1b0d134f0eaa2c5de3a8e17fb
MarinaMoralesFlores <marinamorales98@gmail.com>
2023-07-30 22:35:22 +0200

    sesion

--- HTML/JavaScript Beginner/assets/JavaScript/Sesion18.js
+++ HTML/JavaScript Beginner/assets/JavaScript/Sesion18.js
@@ -1,27 +1,30 @@
-//cuenta atrás
-let number = 10;
-while (number > 0) {
+// //cuenta atrás
+// let number = 10;
+// while (number > 0) {
     
-    number--;
-    console.log(number);
-}
+//     number--;
+//     console.log(number);
+// }
 
-let tabla = 0;
-let multiplicador = 0;
-while (tabla < 20 && multiplicador <10) {
-    tabla += 2;
-    multiplicador++;
-    console.log ("2 x",+multiplicador + " =", +tabla);
-}
+// let tabla = 0;
+// let multiplicador = 0;
+// while (tabla < 20 && multiplicador <10) {
+//     tabla += 2;
+//     multiplicador++;
+//     console.log ("2 x",+multiplicador + " =", +tabla);
+// }
 
 console.log("Tabla del 10")
-let tabladiez = 10;
-let multiplicadordiez = 0;
-while (tabladiez <100 && multiplicadordiez <10) {
-    tabladiez +=10;
-    multiplicadordiez++;
-    console.log("10 x", multiplicadordiez + " = " +tabladiez);
+
+for (let number = 1; number <=10; number++)
+console.log("Tabla del:", number);
+
+let multi= 1;
+while ( multi <= 10) {
+    console.log(number, "x", multi, "=", number * multi);
+    multi++;
 }
+
 
 //anidar bucles
 
